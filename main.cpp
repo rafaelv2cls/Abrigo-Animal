89d61c67e14d99df1a225bdc3b048e2d133135f2
Rafael <rafaellcosta444@gmail.com>
2026-01-08 20:48:44 -0300

    Primeiro Commit

--- main.cpp
+++ main.cpp
@@ -25,11 +25,40 @@ void listagem();
 void cadastro();
 void edicao();
 void remocao();
+void gravacao();
 
 
 int main(){
+	ifstream arq("animais.csv");
+	int i = 0, capacidade = 40;
+	string aux;
+	animal* bancoDeDados = new animal[capacidade];
 	
-    return 0;
+	while (getline(arq, aux, ';')){
+
+		if (i == capacidade){
+			animal* bancoAux = new animal[capacidade + 5];
+			for (int j = 0; j < capacidade; j++){
+				bancoAux[j] = bancoDeDados[j];
+			}
+			delete[] bancoDeDados;
+			bancoDeDados = bancoAux;
+			capacidade += 5;
+		}
+
+		bancoDeDados[i].id = stoi(aux);
+		getline(arq, bancoDeDados[i].nome, ';');
+		getline(arq, bancoDeDados[i].especie, ';');
+		getline(arq, aux, ';');
+		bancoDeDados[i].idade = stoi(aux);
+		getline(arq, aux, ';');
+		bancoDeDados[i].peso = stof(aux);
+		getline(arq, bancoDeDados[i].descricao);
+		
+		i++;
+	}
+
+	return 0;
 }
 
 void buscaId(){
@@ -45,4 +74,6 @@ void cadastro(){
 void edicao(){
 }
 void remocao(){
+}
+void gravacao(){
 }
