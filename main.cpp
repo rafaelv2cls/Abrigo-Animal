bb5553a815b6745f55c6cbc924b4955b6a00f75b
Rafael <rafaellcosta444@gmail.com>
2026-01-13 18:33:37 -0300

    Implementação de Sistemas de Busca por id e Listagem

--- main.cpp
+++ main.cpp
@@ -13,18 +13,18 @@ typedef struct Animal {
     int id;
     string nome;
     string especie;
-    int idade;
+	char sexo;
+    int idade; // em meses
     float peso;
     string descricao;
 	string vacinas;
 } animal;
 
-void busca();
-void buscaId();
-void buscaNome();
-void buscaEspecie();
-void buscaVacina();
-void listagem();
+void printAnimal(animal* &bancoDeDados, int n);
+void busca(animal* &bancoDeDados, int qnt);
+void buscaId(animal* &bancoDeDados);
+void buscaNome(animal* &bancoDeDados, int qnt);
+void listagem(animal* &bancoDeDados, int qnt);
 void cadastro();
 void edicao();
 void remocao();
@@ -53,10 +53,13 @@ int main(){
 		getline(arq, bancoDeDados[i].nome, ';');
 		getline(arq, bancoDeDados[i].especie, ';');
 		getline(arq, aux, ';');
+		bancoDeDados[i].sexo = aux[0];
+		getline(arq, aux, ';');
 		bancoDeDados[i].idade = stoi(aux);
 		getline(arq, aux, ';');
 		bancoDeDados[i].peso = stof(aux);
-		getline(arq, bancoDeDados[i].descricao);
+		getline(arq, bancoDeDados[i].descricao, ';');
+		getline(arq, bancoDeDados[i].vacinas);
 		
 		i++;
 	}
@@ -79,10 +82,10 @@ int main(){
 		cin >> x;
 		switch (x) {
 			case 1:
-				listagem();
+				listagem(bancoDeDados, i);
 				break;
 			case 2:
-				busca();
+				busca(bancoDeDados, i);
 				break;
 			case 3:
 				cadastro();
@@ -107,52 +110,83 @@ int main(){
 	return 0;
 }
 
-void busca(){
+void printAnimal(animal* &bancoDeDados, int n){
+	cout << bancoDeDados[n].id << ". "<< bancoDeDados[n].nome << endl;
+	cout << bancoDeDados[n].especie << ", ";
+	(bancoDeDados[n].sexo == 'M') ? cout << "Macho" << endl : cout << "Fêmea" << endl;
+	// lógica para impressão da idade do animal
+	if (bancoDeDados[n].idade <= 12) {
+		cout << bancoDeDados[n].idade << " meses" << endl;
+	} else {
+		cout << bancoDeDados[n].idade/12 << " ano";
+		if (bancoDeDados[n].idade >= 24) cout << "s";
+		if (bancoDeDados[n].idade%12 != 0) cout << " e " << bancoDeDados[n].idade%12 << " mes";
+		if (bancoDeDados[n].idade%12 > 1) {
+			cout << "es" << endl;
+		} else cout << endl;
+	}
+	
+	cout << bancoDeDados[n].peso << " Kg" << endl;
+	cout << bancoDeDados[n].descricao << endl;
+	cout << "Vacinas: " << bancoDeDados[n].vacinas << endl;
+}
+
+void busca(animal* &bancoDeDados, int qnt){
 	cout << endl << "=============================================" << endl;
 	cout << "1. Busca por ID" << endl;
 	cout << "2. Busca por nome" << endl;
-	cout << "3. Busca por espécie" << endl;
-	cout << "4. Busca por vacina" << endl;
 	cout << "0. Cancelar" << endl;
 	cout << "=============================================" << endl;
 	int n;
 	cin >> n;
 	switch (n) {
 		case 1:
-			buscaId();
+			buscaId(bancoDeDados);
 			break;
 		case 2:
-			buscaNome();
-			break;
-		case 3:
-			buscaEspecie();
-			break;
-		case 4:
-			buscaVacina();
+			buscaNome(bancoDeDados, qnt);
 			break;
 		case 0:
 			break;
 		default:
 			cout << endl << "Entrada inválida, tente novamente." << endl << endl;
-			busca();
+			busca(bancoDeDados, qnt);
 			break;
 	}
 }
-void buscaId(){
+
+void buscaId(animal* &bancoDeDados){
+	int n;
+	cout << endl << "Insira o id:" << endl;
+	cin >> n;
+
+	cout << endl << "=============================================" << endl;
+	printAnimal(bancoDeDados, n - 1);
+	cout << "=============================================" << endl;
 }
-void buscaNome(){
+
+void buscaNome(animal* &bancoDeDados, int qnt){
+	
 }
-void buscaEspecie(){
+
+void listagem(animal* &bancoDeDados, int qnt){
+	cout << endl << "=============================================" << endl;
+	for(int n = 0; n < qnt;n++){
+		cout << "_____________________________________________" << endl;
+		printAnimal(bancoDeDados, n);
+		cout << "_____________________________________________" << endl;
+	}
+	cout << "=============================================" << endl;
 }
-void buscaVacina(){
-}
-void listagem(){
-}
+
 void cadastro(){
 }
+
 void edicao(){
 }
+
 void remocao(){
 }
+
 void gravacao(){
 }
